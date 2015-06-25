49132e7ca2785d0b2c1d45470cd02ab5168fe4f7
Daniel Wagenaar <wagenadl@uc.edu>
2015-06-24 21:38:09 -0400

    Changed (a long time ago) how ss_assign.m uses ss_centroids. Do not ask why.

--- lib/ephys/ss_assign.m
+++ lib/ephys/ss_assign.m
@@ -5,14 +5,23 @@ function spk = ss_assign(spk, master, cutoff)
 %    Distance to centroid is returned as well.
 %    spk = SS_ASSIGN(spk, master, cutoff) only assigns spikes with normalized
 %    distance less than CUTOFF.
-
+%
+%    An alternative would be to normalize the distances by cluster spread,
+%    or even better, assign based on Bayesian inference. If I can estimate
+%    the likelihood that a given waveform W is produced by a cluster K as
+%    p(W|K), then I can calculate p(K|W) = p(W|K) p(K) / p(W).
+%    Here I can the number of spikes in cluster K divided by the total
+%    number of spikes as p(K), and I can use p(W) = sum_K p(W|K).
+%    The trouble is a robust estimate for p(W|K). This is by no means
+%    a trivial operation. I could look at each time point in the template
+%    separately, and calculate p(W|K) = prod_t p(W_t|K), but in fact the
+%    time points are probably strongly correlated, so p(W_t) doesn't 
+%    factorize like that. So then I might as well 
 if nargin<3
   cutoff=inf;
 end
 
-[spk.centroids, svar] = ss_centroids(master);
-spk.cvar = svar.sw;
-
+[spk.centroids, spk.cvar] = ss_centroids(master);
 [N T]=size(spk.waveforms);
 
 qq = find(~isnan(spk.cvar)); R=length(qq);
