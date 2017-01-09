7f3e22226f6bff5278b35e3388c80a30b36599a5
Daniel A. Wagenaar <daw@caltech.edu>
2017-01-09 11:48:26 -0800

    zmxtracer can now group by theta if requested

--- lib/zmxtracer/tracer_drawrays.m
+++ lib/zmxtracer/tracer_drawrays.m
@@ -1,8 +1,9 @@
-function tracer_drawrays(trc, x)
+function tracer_drawrays(trc, x, altc)
 % TRACER_DRAWRAYS - Draw traced rays
 %    TRACER_DRAWRAYS(trc) draws the rays from the most recent
 %    call to TRACER_TRACE. 
 %    TRACER_DRAWRAYS(trc, x) specifies an endpoint differing from the image.
+%    TRACER_DRAWRAYS(trc, x, 1) uses colors based on theta rather than y.
 
 if nargin<2
   x = tracer_imagex(trc);
@@ -13,6 +14,9 @@ if nargin<2
 else
   ximg = [];
   xspec = x;
+end
+if nargin<3
+  altc = 0;
 end
 
 [xx, yy] = tracer_rays(trc);
@@ -25,12 +29,16 @@ if isinf(xmax)
   xmax = 100;
 end
 
+cct = djet(T);
 cc = djet(Y);
 for y=1:Y
-  if Y>1
-    qpen(cc(y,:), 0);
+  if Y>1 && ~altc
+    qpen(cc(y,:));
   end
   for t=1:T
+    if T>1 && altc
+      qpen(cc(t,:));
+    end
     x_ = xx(:,y,t);
     y_ = yy(:,y,t);
     x_(abs(imag(x_))>1e-5) = nan;
