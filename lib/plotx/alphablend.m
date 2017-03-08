a5f0e583c10f4576682bb65846b3010f5e9c3a6b
Daniel A. Wagenaar <daw@caltech.edu>
2017-03-07 16:29:42 -0800

    Improvements to alphablend

--- lib/plotx/alphablend.m
+++ lib/plotx/alphablend.m
@@ -5,22 +5,28 @@ function cc = alphablend(aa, bb)
 %   If AA is (Y,X,3), the output will also be (Y,X,3) and the alpha channel
 %   for AA will be assumed to equal 1 resulting in an output alpha that
 %   also equals one.
+%   It is also permitted for AA and BB to be (Y,X,2) GA images, and 
+%   AA may be (Y,X,1).
 
 % Equations derived in notebook C7:p35 and also on wikipedia.
 
 Sa = size(aa);
 Sb = size(bb);
-Ca = Sa(end);
-Cb = Sb(end);
-if Cb~=4
-  error('alphablend: BB must be YxXx4');
-end
-if Ca<3 || Ca>4
-  error('alphablend: AA must be YxXx3 or YxXx4');  
+if length(Sa)==2
+  Sa(3) = 1;
 end
 
-Sa0 = prod(Sa(1:end-1));
-Sb0 = prod(Sb(1:end-1));
+Ca = Sa(end);
+Cb = Sb(end);
+if length(Sb)~=3 || (Cb~=4 && Cb~=2)
+  error('alphablend: BB must be YxXx4 or YxXx2');
+end
+if length(Sa)~=3 || Ca<1 || Ca>4
+  error('alphablend: AA must be YxXx{1, 2, 3, or 4}');
+end
+
+Sa0 = prod(Sa(1:2));
+Sb0 = prod(Sb(1:2));
 if Sa0~=Sb0
   error('alphablend: AA and BB must have same YxX');
 end
@@ -28,24 +34,40 @@ end
 aa = reshape(aa, [Sa0 Ca]);
 bb = reshape(bb, [Sb0 Cb]);
 
-acol = aa(:,1:3);
-bcol = bb(:,1:3);
-balph = bb(:,4);
+if Ca>=3
+  acol = aa(:,1:3);
+else
+  acol = aa(:,1);
+end
+
+if Cb>=3
+  bcol = bb(:,1:3);
+else
+  bcol = bb(:,1);
+end
+balph = bb(:,end);
+
+Cb1 = size(bcol, 2);
+Ca1 = size(acol, 2);
+
+if Ca1~=Cb1
+  error('alphablend: Either AA and BB must both be RGB, or both grey');
+end
 
 cc = zeros(Sa0, Ca);
-if Ca==3
+if Ca==3 || Ca==1
   % Simple case: opaque background
-  for k=1:3
+  for k=1:Ca1
     cc(:,k) = balph.*bcol(:,k) + (1-balph).*acol(:,k);
   end
 else
   % Full case: semi-opaque background
-  aalph = aa(:,4);
+  aalph = aa(:,end);
   calph = balph + (1-balph).*aalph;
-  for k=1:3
+  for k=1:Ca1
     cc(:,k) = (balph.*bcol(:,k) + (1-balph).*aalph.*acol(:,k)) ./ calph;
   end
-  cc(:,4) = calph;
+  cc(:,Ca) = calph;
 end
 
 cc = reshape(cc, Sa);
